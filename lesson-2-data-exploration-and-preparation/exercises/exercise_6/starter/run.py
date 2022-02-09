21ecd9ac91ad2b214e0921d938d2d9d8c58a5cb2
Nelson Cárdenas Bolaño <nelsoncardenas28@gmail.com>
2022-02-09 07:32:50 -0500

    Add exercise 6 solution

--- lesson-2-data-exploration-and-preparation/exercises/exercise_6/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_6/starter/run.py
@@ -30,8 +30,14 @@ def go(args):
     ###################################
     # COMPLETE the following line     #
     ###################################
+    splits["train"], splits["test"] = train_test_split(
+        df,
+        test_size=args.test_size,
+        stratify=df[args.stratify] if args.stratify != "null" else None,
+        random_state=args.random_state,
+    )
 
-    splits["train"], splits["test"] = # USE train_test_split here to split df according to the provided args.test_size
+    # USE train_test_split here to split df according to the provided args.test_size
 
     # Now we save the artifacts. We use a temporary directory so we do not leave
     # any trace behind
@@ -84,28 +90,31 @@ if __name__ == "__main__":
         "--artifact_root",
         type=str,
         help="Root for the names of the produced artifacts. The script will produce 2 artifacts: "
-             "{root}_train.csv and {root}_test.csv",
+        "{root}_train.csv and {root}_test.csv",
         required=True,
     )
 
     parser.add_argument(
-        "--artifact_type", type=str, help="Type for the produced artifacts", required=True
+        "--artifact_type",
+        type=str,
+        help="Type for the produced artifacts",
+        required=True,
     )
 
     parser.add_argument(
         "--test_size",
         help="Fraction of dataset or number of items to include in the test split",
         type=float,
-        required=True
+        required=True,
     )
 
     parser.add_argument(
         "--random_state",
         help="An integer number to use to init the random number generator. It ensures repeatibility in the"
-             "splitting",
+        "splitting",
         type=int,
         required=False,
-        default=42
+        default=42,
     )
 
     parser.add_argument(
@@ -113,7 +122,7 @@ if __name__ == "__main__":
         help="If set, it is the name of a column to use for stratified splitting",
         type=str,
         required=False,
-        default='null'  # unfortunately mlflow does not support well optional parameters
+        default="null",  # unfortunately mlflow does not support well optional parameters
     )
 
     args = parser.parse_args()
